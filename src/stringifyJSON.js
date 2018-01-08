7d40622458a18696686d5f56db91361a47c447ac
Becca Stanger <beccastanger@gmail.com>
2018-01-08 07:52:32 -0800

    Pass first few tests of stringify
    This work helps stringifyJSON pass some of the
    initial tests for primitive types - numbers,
    booleans, etc.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,21 @@
 
 // but you don't so you're going to write it from scratch:
 
+var stringOutput = "";
+
 var stringifyJSON = function(obj) {
   // your code goes here
+  console.log("Object: " + obj);
+
+  // Handle strings and numbers
+  if(typeof obj === "string") return;
+  if(typeof obj === "number") return '' + number;
+  if(obj === undefined || obj === null || typeof obj === "boolean") return '' + obj;
+
+  // var objLength = obj.length;
+  // console.log("Length: " + objLength);
+
+  // var stringOfIndexVal = obj[objLength - 1].toString();
+  // stringOutput = stringOutput + stringOfIndexVal;
+  // return stringOutput;
 };
