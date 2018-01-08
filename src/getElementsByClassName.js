368d725b17de4628cc0f4958c24d25dd38924117
Becca Stanger <beccastanger@gmail.com>
2018-01-08 11:20:43 -0800

    Pass all tests for getElementsByClassName
    This work makes sure to also look for child nodes
    within child nodes that might match the class name.
    I am concerned though that this solution is
    inefficient since it creates a large array first
    and then evaluates it.  It would probably be more
    efficient to evaluate while creating the list...

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,18 +5,27 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className) {
+
+	// Create helper list of all nodes
 	var bodyEl = document.body;
 	var childNodesEls = [bodyEl];
-	bodyEl.childNodes.forEach(function(node) {
-		childNodesEls.push(node);
-	});
-	
+	var findChildNodes = function(childNodesList){
+		childNodesList.forEach(function(node) {
+			if(node.nodeType === 1){
+				childNodesEls.push(node);
+				findChildNodes(node.childNodes);
+			}
+		});
+	};
+	findChildNodes(bodyEl.childNodes);
+
+	// Return all nodes with class name
 	var elsWithClassName = [];
 	var checkForClassName = function(nodesList){
 		var lastNode = nodesList[nodesList.length - 1];
 		var lastNodeClasses = lastNode.classList;
 		if(lastNodeClasses && lastNodeClasses.contains(className)){
-			elsWithClassName.push(lastNode);
+			elsWithClassName.unshift(lastNode);
 		}
 		nodesList.pop();
 		if(childNodesEls.length){
