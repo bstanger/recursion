bc86aaf1d42d0cc8205dc974be9de85574b0f7ec
Becca Stanger <beccastanger@gmail.com>
2018-01-08 09:33:14 -0800

    Handle objects in stringifyJSON
    Handle empty arrays as base case.  Add recursive
    case that iterates through the keys and creates a
    string composed of the string version of the key,
    a : and the string version of the key-value. Store
    these key value strings in an array and then return
    the array with a { and }.  If the stringified key or
    value is undefined or a function, remove the key
    value string.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,28 +5,44 @@
 
 var stringifyJSON = function(obj) {
 
-  let arrayContent = [];
-
   // Handle strings, numbers, booleans, null, undefined
   if(typeof obj === "string") {
   	return '"' + obj + '"';
   } else if (typeof obj === "number" || typeof obj === undefined || obj === null || typeof obj === "boolean") {
   	return '' + obj;
   }
-
   
   if(typeof obj === "object"){
 
   	// Handle arrays
   	if(Array.isArray(obj)){
-  		if(obj.length == 0) {
+  		if(obj.length == 0) { // Base case
   			return "[]";
-  		} else {
-  			obj.forEach(function(item){
-  				arrayContent.push(stringifyJSON(item));
-  			});
-  			return "[" + arrayContent + "]";
   		}
+		var arrayContent = obj.map(function(arrayItem){ // Recursive case
+			return stringifyJSON(arrayItem);
+		});
+		return "[" + arrayContent + "]";
+
+  	// Handle objects
+  	} else {
+  		if (_.isEmpty(obj)) { // Base case
+  			return "{}";
+  		}
+  		var objContainer = [];
+  		var keyValueString = "";
+
+  		for (var objKey in obj){ // Recursive case
+  			var objKeyValue = obj[objKey];
+  			keyValueString = stringifyJSON(objKey) + ":" + stringifyJSON(objKeyValue);
+  			objContainer.push(keyValueString);
+  			
+  			if(stringifyJSON(objKey) === undefined || stringifyJSON(objKeyValue) === undefined ||
+  				typeof objKey === "function" || typeof objKeyValue === "function"){
+  					objContainer.pop();
+  			}
+  		};
+  		return "{" + objContainer + "}";
   	}
   }
 };
