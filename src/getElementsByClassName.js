84d9337af57026b5fe3e9138081eee537a84a327
Becca Stanger <beccastanger@gmail.com>
2018-01-08 10:57:44 -0800

    Bulk of getElementsByClassName
    This work gets close to returning the expected
    value I believe.  It creates an array of the child
    nodes and then iterates through each with the last
    index and pop, adding the node to a helper array
    if it contains the desired class name.  A problem
    still exists with matching the result to expected,
    possibly due to order in array?

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,31 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+	var bodyEl = document.body;
+	var childNodesEls = [bodyEl];
+	bodyEl.childNodes.forEach(function(node) {
+		childNodesEls.push(node);
+	});
+	
+	var elsWithClassName = [];
+	var checkForClassName = function(nodesList){
+		var lastNode = nodesList[nodesList.length - 1];
+		var lastNodeClasses = lastNode.classList;
+		if(lastNodeClasses && lastNodeClasses.contains(className)){
+			elsWithClassName.push(lastNode);
+		}
+		nodesList.pop();
+		if(childNodesEls.length){
+			checkForClassName(childNodesEls);
+		} else {
+			return elsWithClassName;
+		}
+	};
+
+	if(childNodesEls.length){
+		checkForClassName(childNodesEls);
+		return elsWithClassName;
+	}
+
 };
